29b45e5ebfb02b8829daf0f676372bbcfc7a4dd0
Walter Cordoba <waltercordobadev@gmail.com>
2023-11-07 09:46:00 -0300

    cambios footer

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
-
+router.get('/nosotros', function(req, res, next) {
+  res.render('nosotros');
+})
+router.get('/kinesiologia', function(req, res, next) {
+  res.render('kinesiologia');
+})
 module.exports = router;
