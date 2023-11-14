594e89cb933d0108802f51a6a3addac623150e18
Walter Cordoba <waltercordobadev@gmail.com>
2023-11-13 21:03:08 -0300

    testeo finales, cambios de fondo

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,10 @@ router.get('/nosotros', function(req, res, next) {
 router.get('/kinesiologia', function(req, res, next) {
   res.render('kinesiologia');
 })
+router.get('/ortopedia', function(req, res, next) {
+  res.render('ortopedia');
+})
+router.get('/plantillas', function(req, res, next) {
+  res.render('plantillas');
+})
 module.exports = router;
